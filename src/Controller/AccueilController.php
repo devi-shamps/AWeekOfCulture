564c8d82fec90cbc0c6dc138a1966b487a8f861d
devi-shamps <dimitri.dechamp@gmail.com>
2023-09-29 13:39:27 +0200

    Init

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\TestFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilController extends AbstractController
+{
+    #[Route('/accueil', name: 'app_accueil')]
+    public function index(Request $request): Response
+    {
+
+        return $this->render('accueil/index.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+}
