2949356bfc840bdcfd12e70e09d2cafe54417e7f
Dimitri <dimitri.dechamp@gmail.com>
2023-11-30 10:33:35 +0100

    Pour Azure

--- config/bundles.php
+++ config/bundles.php
@@ -14,5 +14,4 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
